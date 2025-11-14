e92564b7a59cc90b9f9371baa7a129a19f2fe10c
amanuel912 <amanu.hk82@gmail.com>
2025-11-14 12:47:03 +0900

    basic arthimietic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,9 @@
-def sum(n1,n2):
-    print (n1 + n2)
-    
+number1 = 10
+number2 = 5
+print(f"Addition of 10 and 5 is {number1 + number2}")
+print(f"Subtraction of 10 and 5 is {number1 - number2}")
+print(f"Multiplication of 10 and 5 is {number1 * number2}")
 
-sum(4,5)
 
 
 
