5386f85446e43ea4a773d5f360499a8488c1e4c9
amanuel912 <amanu.hk82@gmail.com>
2025-11-14 13:21:00 +0900

    regex friendly 2

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-income = float(input("Enter your monthly income: "))
-expense = float(input("Enter your total monthly expense: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = income - expense
+monthly_savings = monthly_income - monthly_expenses
 
 projected_saving = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 
