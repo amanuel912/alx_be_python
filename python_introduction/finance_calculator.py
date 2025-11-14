c2bed0d6b0c547db17f5071434f4948f06c8806b
amanuel912 <amanu.hk82@gmail.com>
2025-11-14 13:20:02 +0900

    regex friendely code 1

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,9 @@
-income = float(input("Enter your monthly income:"))
-expense = float(input("Enter your total monthly expense"))
+income = float(input("Enter your monthly income: "))
+expense = float(input("Enter your total monthly expense: "))
+
 monthly_savings = income - expense
-projected_saving = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+
+projected_saving = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
+
 print(f"Your monthly savings are {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {projected_saving}")
