e39c298afeaf21a1c8e15f95199bf4f08e69dd0d
amanuel912 <amanu.hk82@gmail.com>
2025-11-23 20:50:02 +0900

    Improve input prompt for adding days in calculate_future_date function

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,7 +5,7 @@ def display_current_datetime():
     print("Current Date and Time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 def calculate_future_date():
-    Days_to_add = int(input("Enter number of days to add: "))
+    Days_to_add = int(input("Enter number of days to add to the current date: "))
     future_date = datetime.now() + timedelta(days=Days_to_add)
     print("Future Date after adding", Days_to_add, "days:", future_date.strftime("%Y-%m-%d %H:%M:%S"))
 
