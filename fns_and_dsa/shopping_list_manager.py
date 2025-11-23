92acf9c924b6358aafd2f43ca24a71c0588185e8
amanuel912 <amanu.hk82@gmail.com>
2025-11-23 20:19:15 +0900

    fixed input propmt text

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the name of the item")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             item = input("Enter item name to remove it")
