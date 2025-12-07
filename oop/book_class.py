8f8f24311bfa4cfb7778d76fd09f2dbf72da2163
amanuel912 <amanu.hk82@gmail.com>
2025-12-07 17:57:06 +0900

    Fix class definition and string representation methods in Book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,11 +1,14 @@
-class Book():
+class Book:
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
-    def _str(self):
-        return"{self.title} by {self.author}, published in {self.year}"
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
     def __repr__(self):
-        return"Book(title='{self.title}', author='{self.author}', year={self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
     def __del__(self):
-        print(f"Deleting : {self.title}")
+        print(f"Deleting {self.title}")
